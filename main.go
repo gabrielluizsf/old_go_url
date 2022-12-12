5602fafd77a8b83eecfc9655dae9b935bc7cffd9
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-12-11 23:47:44 -0300

    main file

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/GabrielLuizSF/go_url/pkg/system/analyzer"
+	"github.com/GabrielLuizSF/go_url/pkg/version"
+)
+
+func main(){
+	version.PrintVersion();
+    analyzer.OSAnalyzer();
+
+	
+}
+
+
