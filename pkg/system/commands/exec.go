839b381d3a790556e85821a2d1033b1313222868
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-07 14:39:01 -0300

    Move the println for package warning

--- pkg/system/commands/exec.go
+++ pkg/system/commands/exec.go
@@ -9,7 +9,6 @@ import (
 func Execute(command string){
 	out, err := exec.Command(command).Output();
 		warning.PRINT_DEFAULT_ERRORS(err,"INVALID COMMAND");
-		fmt.Println("[Command Successfully Executed]");
 	output := string(out[:])
 	fmt.Println(output)
 }
--- warning/errors.go
+++ warning/errors.go
@@ -5,5 +5,7 @@ import "fmt"
 func PRINT_DEFAULT_ERRORS(ERROR error,MESSAGE string){
 	if ERROR != nil{
 	fmt.Println("[MESSAGE]:",MESSAGE,"\n[ERROR]:",ERROR);
-	}
+	}else{
+    fmt.Println("[Command successfully executed]");
+  }
 }
