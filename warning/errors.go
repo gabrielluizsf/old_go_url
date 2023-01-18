90d2683f6df404f8a04eb154d8f37b0c5bd014eb
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-18 16:37:53 -0300

    Adding function explanation in a comment

--- warning/errors.go
+++ warning/errors.go
@@ -2,6 +2,9 @@ package warning
 
 import "fmt"
 
+// This function prints the errors received by parameter,
+//along with the message received by parameter, 
+//generating a customized error message for the console.
 func PRINT_DEFAULT_ERRORS(ERROR error,MESSAGE string){
 	if ERROR != nil{
 	fmt.Println("[MESSAGE]:",MESSAGE,"\n[ERROR]:",ERROR);
