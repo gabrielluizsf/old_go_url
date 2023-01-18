5d4b251635a383ecf37897b4d30e4aae7f0ce469
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-18 17:02:40 -0300

    Adding the function explanation in comment

--- pkg/system/analyzer/myos.go
+++ pkg/system/analyzer/myos.go
@@ -3,10 +3,11 @@ package analyzer
 import (
 	"runtime"
 )
-
+//this function returns the operating system name
 func MyOS()string{
 	return runtime.GOOS;
 }
+//this function returns the architecture of the system
 func MyARCH()string{
 	return runtime.GOARCH;
 }
