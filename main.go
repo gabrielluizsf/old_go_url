90e099b2e3ff8bc749e2d7b7680efc60c61a326d
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-18 16:36:09 -0300

    Adding License

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"github.com/GabrielLuizSF/go_url/pkg/version"
 )
 
+// Copyright 2023 gabrielluizsf. All rights reserved
+// Use of this source code is governed by MIT
+// license that can be found in the LICENSE file.
+
 func main(){
 	version.PrintVersion();
     analyzer.OSAnalyzer();
-
-	
 }
 
 
